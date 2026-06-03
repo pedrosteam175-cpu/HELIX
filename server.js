12cb6828ef44319f4409874d428b9d62b67c2463
pedrosteam175-cpu <pedrosteam175@gmail.com>
2026-06-03 14:10:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,14 +5,20 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+// servir arquivos da pasta public
 app.use(express.static(path.join(__dirname, "public")));
 
+// abrir index.html
 app.get("/", (req, res) => {
   res.sendFile(
-    path.join(__dirname, "public", "index.html")
+    path.join(
+      __dirname,
+      "public",
+      "index.html"
+    )
   );
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log("Servidor iniciado");
+  console.log("HELIX iniciado");
 });
