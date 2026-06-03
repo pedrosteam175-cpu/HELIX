c9f6ff4f70ef2ce6b1e0064395457e28dd9adfbb
pedrosteam175-cpu <pedrosteam175@gmail.com>
2026-06-03 14:09:16 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-
 const express = require("express");
-const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.get("/", (req, res) => {
-  res.send("HELIX ONLINE");
+  res.sendFile(
+    path.join(__dirname, "public", "index.html")
+  );
 });
 
 app.listen(PORT, "0.0.0.0", () => {
-  console.log(`Servidor rodando na porta ${PORT}`);
+  console.log("Servidor iniciado");
 });
